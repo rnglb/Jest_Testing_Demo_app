e912ba15bb46dbd52e138642a5e5ae5b18c94711
rnglb <kashakkumari5@gmail.com>
2022-06-17 22:21:42 +0530

    code update

--- function.test.js
+++ function.test.js
@@ -1,15 +1,20 @@
 const functions= require("./functions");
 
-test("test first",()=>{
+test("test 1",()=>{
     expect(functions.add(2,3)).toBe(5);
 });
-test("test second",()=>{
+test("test 2",()=>{
     expect(functions.isNull()).not.toBe(6);
     });
-test("test third",()=>{
+test("test 3",()=>{
     expect(functions.isNull()).toBeNull();
     });
-test("test fourth",()=>{
-    //expect(functions.checkValue(null/undefiend/0)).toBeFalsy();
+test("test 4",()=>{
     expect(functions.checkValue(null)).toBeFalsy();
+    });
+test("test 5",()=>{
+     expect(functions.checkValue(undefiend)).toBeFalsy();
+     });
+test("test 6",()=>{
+    expect(functions.checkValue(0)).toBeFalsy();
     });
